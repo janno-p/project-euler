3a79b2efc4a76a5113038a9b60a44dd91e199388
Janno Põldma <janno.poldma@gmail.com>
2011-07-25 22:44:50 +0300

    Problem 81: final solution

--- problem81.rb
+++ problem81.rb
@@ -6,23 +6,15 @@
 # bottom right, by only moving to the right and down, is indicated in bold red
 # and is equal to 2427.
 #
-#   [131]  673   234   103    18
-#   [201] [ 96] [342]  965   150
-#    630   803  [746] [422]  111
-#    537   699   497  [121]  956
-#    805   732   524  [ 37] [331]
+#   131* 673  234  103   18
+#   201*  96* 342* 965  150
+#   630  803  746* 422* 111
+#   537  699  497  121* 956
+#   805  732  524   37* 331*
 #
 # Find the minimal path sum, in matrix.txt (right click and 'Save Link/Target
 # As...'), a 31K text file containing a 80 by 80 matrix, from the top left to
 # the bottom right by only moving right and down.
-
-test_matrix = [
-  [131, 673, 234, 103, 18],
-  [201, 96, 342, 965, 150],
-  [630, 803, 746, 422, 111],
-  [537, 699, 497, 121, 956],
-  [805, 732, 524, 37, 331]
-]
 
 matrix = []
 File.open('data/problem81_matrix.txt', 'r') do |file|
@@ -31,30 +23,17 @@ File.open('data/problem81_matrix.txt', 'r') do |file|
   end
 end
 
-# For testing
-matrix = test_matrix
-
-# Assuming we have square matrix
-dimension = matrix.size - 1
-
-x, y, best = 0, 0, matrix[0][0]
-while x < dimension or y < dimension
-  if x == dimension
-    y += 1
-  elsif y == dimension
-    x += 1
-  elsif matrix[x + 1][y] > matrix[x][y + 1]
-    y += 1
-  else
-    x += 1
-  end
-  best += matrix[x][y]
-end
-
-for x in 0...dimension do
-  for y in 0...dimension do
-    
+num_rows = 2 * matrix.size - 1
+for row in 0...num_rows do
+  min_col = [row - matrix.size + 1, 0].max
+  max_col = [row, matrix.size - 1].min
+  for col in min_col..max_col do
+    x, y = col, row - col
+    val1 = x > 0 ? matrix[x - 1][y] : nil
+    val2 = y > 0 ? matrix[x][y - 1] : nil
+    next unless val1 or val2
+    matrix[x][y] += [val1 || val2, val2 || val1].min
   end
 end
 
-puts best
+puts matrix[-1][-1]
