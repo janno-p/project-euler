a08ba98080f58534be36166181da4f0963e1b704
Janno Põldma <janno.poldma@gmail.com>
2011-12-12 13:07:13 +0200

    Problem 99 data file

--- problems/problem51/solution.rb
+++ problems/problem51/solution.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
 
+require File.join(File.dirname(__FILE__), '..', 'common', 'primes.rb')
+
+include Primes
+
+# Need to find eight prime value family, so last number cannot be '*' because
+# there are only 4 combinations for prime numbers: ending with 1, 3, 7 or 9.
+
+rank, rank_max = 0, 0
+result, result_score = 0, 0
+
 def prime?(n)
   max = Math.sqrt(n).to_i
   for i in 2..max
@@ -7,6 +17,64 @@ def prime?(n)
   end
   true
 end
+
+def get_mask(n, rank)
+  num = n.to_s.split(//).map(&:to_i)
+  a = n / 10
+  r = rank - 1
+  total = r ** 2
+  for i in 1...total do
+    x, b = 1, nil
+    can_yield = for j in 0...r do
+      unless i & x == 0 then
+        break false if b != num[j]
+        b = num[j] if b.nil?
+      end
+      x <<= 1
+    end
+    yield i.to_s(2).to_i if can_yield
+  end
+end
+
+primes do |number|
+  while number > rank_max do
+    rank += 1
+    rank_max = 10 ** rank
+    puts "Rank: #{rank}; Max: #{rank_max}; Number: #{number}"
+  end
+  next unless rank > 1
+  if rank == 2 then
+    z, count = (number + 10), 1
+    while z < rank_max do
+      count += 1 if prime?(z)
+      z += 10
+    end
+    if count > result_score then
+      result = number
+      result_score = count
+    end
+  else
+    break
+  end
+end
+
+puts "#{result}/#{result_score}"
+
+exit
+
+primes(100) do |prime|
+  next if prime < 10
+  
+end
+
+
+primes(1_000_000) do |p|
+  puts p
+end
+
+exit
+
+
 
 def create_masks(rank)
   masks = []
