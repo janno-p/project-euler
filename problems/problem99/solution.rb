1f8621f2883454cf06988588b0c14b5af9f1fc10
Janno Põldma <janno.poldma@gmail.com>
2011-12-13 12:38:07 +0200

    Solution for problem 99

--- problems/problem99/solution.rb
+++ problems/problem99/solution.rb
@@ -1,35 +1,15 @@
 #!/usr/bin/env ruby
 
-base_a = 632382
-power_a = 518061
+# If a ** x ~ b ** y is true (where ~ is >, <, ==, etc.)
+# then x * log(a) ~ y * log(b) is also true
 
-base_b = 519432
-power_b = 525806
+line_index, best_index, best_value = 0, 0, 0
 
-def pow(base, power, pots)
-  mask = []
-  current = power
-  pots.each_with_index do |pot, index| # =>
-    new_current = current - pot
-    if new_current >= 0 then
-      mask << pots.size - index - 1
-      current = new_current
-    end
-    break if current == 0
-  end
-  result = 1
-  current = base
-  for i in 0..mask.max do
-    result *= current if mask.include?(i)
-    current *= current
-  end
-  result
-end
-
-max_line = 0
-pots = (0..24).to_a.reverse.map { |i| 2 ** i }
 File.readlines(File.join(File.dirname(__FILE__), '..', 'data', 'base_exp.txt')).each do |line|
+  line_index += 1
   base, power = line.strip.split(',').map(&:to_i)
+  line_value = power * Math.log10(base)
+  best_index, best_value = line_index, line_value if line_value > best_value
 end
 
-puts max_line
+puts best_index
