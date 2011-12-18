6fbf7dc7d1034c069a7ccb09762f17eb0278bd90
Janno Põldma <janno.poldma@gmail.com>
2011-12-18 15:50:21 +0200

    Solution for 243

--- problems/common/fraction.rb
+++ problems/common/fraction.rb
@@ -1,22 +1,56 @@
 require File.join(File.dirname(__FILE__), 'math.rb')
 
 class Fraction
+  include Math
+  
   attr_reader :numerator, :denominator
   
-  def initialize(numerator, denominator)
+  def initialize(numerator, denominator = 1)
+    numerator / denominator
     @numerator = numerator
     @denominator = denominator
     simplify
+  end
+  
+  def <(fraction)
+    @numerator * fraction.denominator < fraction.numerator * @denominator
+  end
+  
+  def +(fraction)
+    Fraction.new(@numerator * fraction.denominator + fraction.numerator * @denominator, @denominator * fraction.denominator)
+  end
+  
+  def -(fraction)
+    Fraction.new(@numerator * fraction.denominator - fraction.numerator * @denominator, @denominator * fraction.denominator)
+  end
+  
+  def *(fraction)
+    Fraction.new(@numerator * fraction.numerator, @denominator * fraction.denominator)
+  end
+  
+  def /(fraction)
+    Fraction.new(@numerator * fraction.denominator, fraction.numerator * @denominator)
   end
   
   def to_f
     @numerator.to_f / @denominator
   end
   
-  private
-  def simplify
-    d = Math::gcd(@numerator, @denominator)
-    @numerator /= d
-    @denominator /= d
+  def to_s
+    return "0" if @numerator == 0
+    return @numerator.to_s if @denominator == 1
+    "#{@numerator}/#{@denominator}"
   end
+  
+  private
+  
+  def simplify
+    d = gcd(@numerator, @denominator)
+    @numerator = (@numerator / d).to_i
+    @denominator = (@denominator / d).to_i
+  end
+  
+  public
+  
+  Zero = Fraction.new(0, 1)
 end
--- problems/problem243/solution.rb
+++ problems/problem243/solution.rb
@@ -1,27 +1,45 @@
 #!/usr/bin/env ruby
 
-require File.join(File.dirname(__FILE__), '..', 'common', 'math.rb')
+require File.join(File.dirname(__FILE__), '..', 'common', 'fraction.rb')
+require File.join(File.dirname(__FILE__), '..', 'common', 'prime_generator.rb')
 
-include Math
-
-def count_resilient_fractions(n)
-  count = 0
-  1.step(n - 1, n % 2 == 0 ? 2 : 1)
-  for i in 1...n do
-    count += 1 if gcd(i, n) == 1
+def prime_ratio(factors)
+  result = Fraction::Zero
+  for n in 1..factors.size do
+    sub_result = Fraction::Zero
+    factors.combination(n).each do |c|
+      sub_result = sub_result + Fraction.new(1, c.inject(:*))
+    end
+    if n % 2 == 0 then
+      result = result - sub_result
+    else
+      result = result + sub_result
+    end
   end
-  count
+  Fraction.new(1) - result
 end
 
-def required_resilience?(d)
-  n = count_resilient_fractions(d)
-  94744 * n < 15499 * (d - 1)
+factors = []
+prime_generator = PrimeGenerator.new
+target_resilience = Fraction.new(15499, 94744)
+
+last_number = 0
+last_resilience = Fraction::Zero
+
+while true do
+  factors << prime_generator.next
+  number = factors.inject(:*)
+  ratio = prime_ratio(factors)
+  resilience = Fraction.new(ratio.numerator * (number / ratio.denominator), number - 1)
+  break if resilience < target_resilience
+  last_number = number
+  last_resilience = resilience
 end
 
-d = 2
-until required_resilience?(d)
-  puts d if d % 1000 == 0
-  d += 1
+for i in 2...factors.max do
+  resilience = Fraction.new(last_resilience.numerator * i, last_number * i - 1)
+  if resilience < target_resilience then
+    puts last_number * i
+    exit
+  end
 end
-
-puts d
